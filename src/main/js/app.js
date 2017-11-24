d4b7acf1bc2f2f585e3efa5248c3f71279d77026
lapots <sebastrident@gmail.com>
2017-11-24 17:02:02 +0300

    Pagination component prototype

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -5,11 +5,28 @@ const ReactDOM = require('react-dom');
 
 class App extends React.Component {
     render() {
-        return <h1>Hello, World</h1>;
+        return <Pagination />
+    }
+}
+
+class Pagination extends React.Component {
+    render() {
+        return <nav aria-label="Page navigation">
+                <ul className="pagination" id="pages"></ul>
+            </nav>
     }
 }
 
 ReactDOM.render(
     <App />,
-    document.getElementById('react')
+    document.getElementById('react'),
+    function() {
+        $('#pages').twbsPagination({
+            totalPages: 10,
+            visiblePages: 4,
+            onPageClick: function(event, page) {
+                console.info(page);
+            }
+        });
+    }
 );
