bdb4d8f955aac96f0313decb692f1111191ed18f
lapots <sebastrident@gmail.com>
2017-11-28 17:10:16 +0300

    Simplified domain for player

---
+++ src/main/java/com/lapots/game/monolith/rest/out/BasicPlayer.java
@@ -0,0 +1,9 @@
+package com.lapots.game.monolith.rest.out;
+
+import lombok.Data;
+
+@Data
+public class BasicPlayer {
+    private String id;
+    private String name;
+}
--- src/main/java/com/lapots/game/monolith/rest/out/DomainUtils.java
+++ src/main/java/com/lapots/game/monolith/rest/out/DomainUtils.java
@@ -3,15 +3,27 @@ package com.lapots.game.monolith.rest.out;
 import com.lapots.game.monolith.domain.player.relational.RPlayer;
 import com.lapots.game.monolith.domain.player.graph.GPlayer;
 
+import java.util.stream.Collectors;
+
 public class DomainUtils {
 
     public static CompletePlayer merge(RPlayer rData, GPlayer gData) {
         CompletePlayer cp = new CompletePlayer();
         cp.setClazz(rData.getClazz());
         cp.setDataId(rData.getId().toString());
-        cp.setConnectionId(gData.getId().toString());
+        cp.setSocialId(gData.getId().toString());
         cp.setLevel(rData.getLevel());
         cp.setName(rData.getName());
+
+        cp.setComrades(
+            gData.getComrades().stream()
+                    .map(comrade -> {
+                        BasicPlayer bp = new BasicPlayer();
+                        bp.setId(comrade.getId().toString());
+                        bp.setName(comrade.getName());
+                        return bp; })
+                    .collect(Collectors.toList())
+        );
         return cp;
     }
 
